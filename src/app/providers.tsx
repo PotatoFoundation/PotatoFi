cde7da5d323beb9103295256c2b86d5214fd358d
Sherly Febrianti <sherlyfebrianti96@yahoo.co.id>
2024-06-16 23:57:33 +0200

    Refactor the ThemeConfig

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,36 +1,36 @@
 import { wagmiConfig } from "@/providers/wagmi/wagmi";
 import { AntdRegistry } from "@ant-design/nextjs-registry";
-import { ConfigProvider } from "antd";
+import { ConfigProvider, ThemeConfig } from "antd";
 import { ReactNode } from "react";
 import { WagmiProvider } from "wagmi";
 
 const mainPotato = "#fabbba";
 
 const AppProviders = ({ children }: { children: ReactNode }) => {
+  const themeConfig: ThemeConfig = {
+    token: {
+      // Seed Token
+      colorPrimary: mainPotato,
+      borderRadius: 2,
+
+      // Alias Token
+      colorBgContainer: "#FFDEDC",
+      colorText: "#AF504D",
+      colorPrimaryBorder: "#D58280",
+    },
+    components: {
+      Menu: {
+        colorPrimary: "#681210",
+        itemBg: "#FFDEDC",
+        itemSelectedBg: mainPotato,
+      },
+    },
+  };
+
   return (
     <AntdRegistry>
-      <ConfigProvider
-        theme={{
-          token: {
-            // Seed Token
-            colorPrimary: mainPotato,
-            borderRadius: 2,
-
-            // Alias Token
-            colorBgContainer: "#FFDEDC",
-            colorText: "#AF504D",
-            colorPrimaryBorder: "#D58280",
-          },
-          components: {
-            Menu: {
-              colorPrimary: "#681210",
-              itemBg: "#FFDEDC",
-              itemSelectedBg: mainPotato,
-            },
-          },
-        }}
-      >
         <WagmiProvider config={wagmiConfig}>{children}</WagmiProvider>
+      <ConfigProvider theme={themeConfig}>
       </ConfigProvider>
     </AntdRegistry>
   );
